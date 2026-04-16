d4d8889fb805255c3bde33199653dd76146f7e39
Clarence Grimaldo <clarencegrimaldo@gmail.com>
2026-04-15 22:02:11 -0400

    Update vite.config.ts

--- portfolio-web/vite.config.ts
+++ portfolio-web/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace 'Portfolio' with your EXACT GitHub repository name
-  base: '/Portfolio/', 
+  // Matches your exact repository name clorece.portfolio
+  base: '/clorece.portfolio/', 
 })
