dbfeb993a4d6478105e13b014585286467f21508
Clarence Grimaldo <clarencegrimaldo@gmail.com>
2026-04-15 21:57:57 -0400

    Setup GitHub Pages deployment

--- portfolio-web/vite.config.ts
+++ portfolio-web/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Useful for GitHub Pages
+  // Replace 'Portfolio' with your EXACT GitHub repository name
+  base: '/Portfolio/', 
 })
